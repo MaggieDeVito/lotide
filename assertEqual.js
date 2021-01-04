c3a85760ec48a885f20102af9f83ee946453af30
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-04 23:24:12 +0000

    Added assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🥳🥳🥳 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🤬🤬🤬 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual(1, 3);
