ea3e42e5d23b698b318edf900df7df68901c77bd
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-06 22:42:43 +0000

    made middle function

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const assertArraysEqual = function(array1, array2, expected) {
+  const actual = eqArrays(array1, array2);
+  if (actual === expected) { // checking if eqArrays is true
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`); //if true console log this
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`); //else console log this
+  }
+};
+
+//the middle function should return an array with only the 
+//middle element(s) of the provided array.
+//This means the length of the returned elements could vary
+
+
+const middle = function(array) {
+
+  let mid = Math.floor(array.length / 2);
+
+  if (array.length <= 2) { // checks array length
+    return []; // returns empty array
+  } else if (array.length > 2 && array.length % 2 != 0){ // checks array length and checks that array is odd
+    console.log("ahhhh"); // test
+    return [array[mid]];
+  } else if (array.length > 2 && array.length % 2 === 0) { // checks array length and checks that array is even
+    console.log("yaaaas") // test
+    return [array[mid - 1], array[mid]];
+  }
+}
+
+console.log(middle([1, 5, 19, 2, 3, 4]));
