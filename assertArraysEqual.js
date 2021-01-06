d83b27b815a207e8914ece9253bb4c3e851ce3ee
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-06 21:40:16 +0000

    updated function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,19 @@
-const eqArrays = function(array1, array2) { 
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) { 
-      return false; 
+    if (array1[i] !== array2[i]) {
+      return false;
     }
   } return true;
 };
 
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays()) {
-    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
+const assertArraysEqual = function(array1, array2, expected) {
+  const actual = eqArrays(array1, array2);
+  if (actual === expected) { // checking if eqArrays is true
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`); //if true console log this
   } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`); //else console log this
   }
 };
