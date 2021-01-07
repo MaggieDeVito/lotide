c9e48b74fc54d99bca15ace8cfd26ce03eae4277
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-07 20:49:14 +0000

    first commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,20 @@
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (let value in object1) {
+    if ((object1[value]) !== (object2[value])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
