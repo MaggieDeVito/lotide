1610534694ec19c02154d3c890b216caf91b38d5
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-08 04:39:56 +0000

    first commit

---
+++ without.js
@@ -0,0 +1,12 @@
+const without = function(source, itemsToRemove) {
+  
+  let newArray = [];
+  itemsToRemove = itemsToRemove.toString();
+  
+  for (let item of source) {
+    if (item !== itemsToRemove) {
+      newArray.push(item);
+    }
+  }
+  return newArray;
+};
