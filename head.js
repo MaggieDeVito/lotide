644ff644edebe822091e883d1ca9bb753ad51fe8
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-11 20:37:37 +0000

    refactored with test

--- head.js
+++ head.js
@@ -1,13 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(data) {
   return data[0];
 };
+
+module.exports = head;
 
