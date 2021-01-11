d616a8cef9b135eb4a16f81548418c399586cde1
MaggieDeVito <maggiemaydevito@gmail.com>
2021-01-11 20:59:28 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,19 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🤬🤬🤬 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(array1, array2) { // has to have parameters otherwise its undefined
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) { // loops through eqArrays parameters (not sure if needed)
     if (array1[i] !== array2[i]) { // checking if array at index 0 is equal to index at 1
-      return false; // if they are equal, return this
+      return false; // if they arent equal, return this
     }
   } return true;
 };
-assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), false);
+
+module.exports = eqArrays;
